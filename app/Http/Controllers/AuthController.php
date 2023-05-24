7efc9bcc9a87c89b66796418a79bd63bf0e010f7
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2023-05-24 21:26:17 +0000

    Authentication flow

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email'=> 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name'=> $fields['name'],
+            'email'=> $fields['email'],
+            'password'=> bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('opktoken')->plainTextToken;
+        $response = [
+            'user'=> $user,
+            'token'=> $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout() {
+        
+        $user = Auth::user();
+        if (!$user) {
+            return response()->json(['message' => 'User not authenticated'], Response::HTTP_UNAUTHORIZED);
+        }
+        
+        $user->tokens()->delete();
+
+        return [
+            'message'=> 'Logged out'
+        ];
+    }
+
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'email'=> 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        //check emaail .. 
+        $user = User::where('email', $fields['email'])->first();
+        
+        //check password
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message'=> 'Incorrect Credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('opktoken')->plainTextToken;
+        $response = [
+            'user'=> $user,
+            'token'=> $token
+        ];
+
+        return response($response, 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group(["prefix" => "v1"], function () {
-    Route::apiResource("empties-receiving-logs", \App\Http\Controllers\API\EmptiesLogController::class);
-    Route::get("empties-returned-logs", [\App\Http\Controllers\API\EmptiesLogController::class, 'getEmptiesReturned']);
-    Route::post("empties-returned-logs", [\App\Http\Controllers\API\EmptiesLogController::class, 'postEmptiesReturned']);
-    Route::apiResource("products_returnable", \App\Http\Controllers\API\ProductController::class);
+    Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+    
+    Route::group(["middleware" => "auth:sanctum"], function () {
+        Route::apiResource("empties-receiving-logs", \App\Http\Controllers\API\EmptiesLogController::class);
+        Route::get("empties-returned-logs", [\App\Http\Controllers\API\EmptiesLogController::class, 'getEmptiesReturned']);
+        Route::post("empties-returned-logs", [\App\Http\Controllers\API\EmptiesLogController::class, 'postEmptiesReturned']);
+        Route::apiResource("products_returnable", \App\Http\Controllers\API\ProductController::class);
+        Route::apiResource("products", \App\Http\Controllers\API\ProductController::class);
+        Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    });
 });
+
+
+
+
