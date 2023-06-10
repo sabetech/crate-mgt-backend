1ef3cc56a7f835d080700fc5eaef8f02d2be20be
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2023-06-10 15:38:29 +0000

    health check

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(["prefix" => "v1"], function () {
     Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
     Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
-    
+    Route::get('/health', function() {
+        return response()->json([
+            "success" => true,
+            "data" => "[GET] Server is up and running"
+        ]);
+    });
+
+    Route::post('/health', function() {
+        return response()->json([
+            "success" => true,
+            "data" => "[POST] Server is up and running"
+        ]);
+    });
+
     Route::group(["middleware" => "auth:sanctum"], function () {
         Route::apiResource("empties-receiving-logs", \App\Http\Controllers\API\EmptiesLogController::class);
         Route::get("empties-returned-logs", [\App\Http\Controllers\API\EmptiesLogController::class, 'getEmptiesReturned']);
