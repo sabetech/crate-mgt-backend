f42c9377600a0f73ae1999314678a2e04be62a74
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2023-11-11 11:13:43 +0000

    Migrations to simplify management

---
+++ database/migrations/2023_11_11_110308_create_empties_balance_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empties_balance', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->foreignId('product_id')->constrained('products');
+            $table->integer('quantity')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empties_balance');
+    }
+};
