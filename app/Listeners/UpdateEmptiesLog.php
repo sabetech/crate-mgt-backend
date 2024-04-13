dd8706c87a2234a24ac22457430eab090fe851cb
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-04-13 15:50:35 +0000

    use products

--- app/Listeners/UpdateEmptiesLog.php
+++ app/Listeners/UpdateEmptiesLog.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 use App\Models\EmptiesReceivingLog;
+use App\Models\Product;
 
 class UpdateEmptiesLog
 {
