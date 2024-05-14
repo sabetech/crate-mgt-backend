e5cfe540622a70ee311b7bdfff64bb02dee9941c
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-05-14 15:49:43 +0000

    export inventory constants

--- app/Listeners/UpdateEmptiesTransaction.php
+++ app/Listeners/UpdateEmptiesTransaction.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 use App\Constants\EmptiesConstants;
+use App\Constants\InventoryConstants;
 use App\Models\EmptiesTransaction;
 
 class UpdateEmptiesTransaction
