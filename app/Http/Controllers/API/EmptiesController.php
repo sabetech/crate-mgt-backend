f32f6e74156bfdd63764d7c23a03bdfebad124ed
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-05-14 14:53:12 +0000

    use Empties Transaction

--- app/Http/Controllers/API/EmptiesController.php
+++ app/Http/Controllers/API/EmptiesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\EmptiesBalance;
+use App\Models\EmptiesTransaction;
 
 class EmptiesController extends Controller
 {
