1ea466845a7d8314dfb141b9392bdec813515f46
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-05-18 15:43:52 +0000

    migration to modify activity field in empties transaction

---
+++ database/migrations/2024_05_18_153906_modify_empties_transactions_table_on_activity_field.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        // Schema::table('empties_transactions', function (Blueprint $table) {
+            // $table->enum('activity', ['customer_empties_return', 'ship_to_ggbl', 'customer_purchase'])->change();
+            DB::statement("ALTER TABLE `empties_transactions` MODIFY `activity` ENUM('customer_empties_return', 'ship_to_ggbl', 'customer_purchase') NOT NULL");
+        // });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
